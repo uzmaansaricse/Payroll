f52ae15ce7509518620a8b4448d67dc503100a85
uzmaansaricse <uzmaasr54@gmail.com>
2025-06-13 14:15:07 +0530

    Updated 2 tabs working

--- Backend/server.js
+++ Backend/server.js
@@ -2,14 +2,15 @@ import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import cors from "cors";
+
 import connectDB from "./config/db.js";
 import superAdminRoutes from "./routes/superAdminRoute.js";
 // server.js (end of file ke upar)
 import initSuperAdmin from "./utils/initSuperAdmin.js";
 import formSchemaRoute from './routes/formSchemaRoute.js';
 import companyRoutes from './routes/companyRoute.js'; // ✅ Added company route
-// CORRECT
-import subSuperAdminRoutes from './routes/subSuperAdminRoute.js';
+
+
 
 
 dotenv.config();
@@ -20,14 +21,12 @@ initSuperAdmin();
 app.use('/api/formschema', formSchemaRoute);
 connectDB();
 app.use("/api/superadmin", superAdminRoutes);
- app.use("/api/sub-super-admin", subSuperAdminRoutes);
 app.use("/api/company", companyRoutes); // ✅ Mounted company route
 
 
 const PORT = process.env.PORT || 5000;
-app.listen(4000, () => {
+app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
-
 });
 
 
