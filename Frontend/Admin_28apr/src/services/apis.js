f79844e5ea3da1c1006a7680f786126fe5e0852d
krishna-65 <sharmakrishnakant721@gmail.com>
2025-06-22 14:22:08 +0530

    add backend url

--- Frontend/Admin_28apr/src/services/apis.js
+++ Frontend/Admin_28apr/src/services/apis.js
@@ -1,5 +1,5 @@
 //const BASE_URL = "http://localhost:4000/api/v1"
-const BASE_URL = "http://localhost:4000/api"
+const BASE_URL = "https://payroll-gujd.onrender.com/api"
 
 // AUTH ENDPOINTS/admins/login
 export const authEndpoints = {
