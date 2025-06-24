1f9208f22c3445c1cdf853a4f9364b4cc0f0819a
krishna-65 <sharmakrishnakant721@gmail.com>
2025-06-24 10:29:12 +0530

    add api

--- Frontend/Super_admin/src/services/apis.js
+++ Frontend/Super_admin/src/services/apis.js
@@ -1,5 +1,5 @@
-const BASE_URL = "http://localhost:4000/api"
-//const BASE_URL = "https://payroll-gujd.onrender.com/api"
+//const BASE_URL = "http://localhost:4000/api"
+const BASE_URL = "https://payroll-gujd.onrender.com/api"
 
 // AUTH ENDPOINTS
 export const superadminendpoints = {
